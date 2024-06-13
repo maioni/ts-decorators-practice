5c928f039621a9d46bdeeaf54b13a395a276dfb0
Daniel Maioni Araújo de Assis <sniper_dmaa@hotmail.com>
2024-06-13 19:02:12 +0100

    Decorations linked

--- Dec.ts
+++ Dec.ts
@@ -2,9 +2,15 @@
 
 
 
+
+const someObject = {
+    someProperty: 'initial'
+}
+
 class Manager {
 
-    @watchChange
+    //@watchChange
+    @linkValue('someProperty')
     someProperty: string;
 
 }
@@ -12,7 +18,39 @@ class Manager {
 const manager: Manager = new Manager();
 
 manager.someProperty = '123';
+console.log('someObject.someProperty: ' + someObject.someProperty);
 manager.someProperty = '456';
+console.log('someObject.someProperty: ' + someObject.someProperty);
+
+function linkValue(otherObject: any) {
+    return function (target: any, key: string) {
+
+        let property = target[key];
+    
+    const getter = () => {
+        return property;
+    }
+
+    const setter = (newVal) => {
+        property = newVal;
+        otherObject[key] = newVal;
+        
+    }
+
+    Object.defineProperty(target, key, {
+        get: getter,
+        set: setter,
+        configurable: true,
+        enumerable: true
+    })
+    }
+}
+
+
+
+
+
+
 
 function watchChange(target: any, key: string) {
      
